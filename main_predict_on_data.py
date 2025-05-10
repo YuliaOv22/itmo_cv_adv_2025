c7307095e192eee8c20ce674631bdc5dce5ec61c
JuliaJu <yulia.ovva@gmail.com>
2025-05-10 12:00:31 +0300

    feat(add_new_results): Добавлены результаты соревнования Kaggle
    Добавлены результаты соревнования Kaggle

--- main_predict_on_data.py
+++ main_predict_on_data.py
@@ -20,17 +20,21 @@ if __name__ == "__main__":
         test_images_path_private = f"{private_data_dir}/test"
         output_dir_path = "output"
         image_extensions = {".jpg", ".jpeg", ".png", ".bmp", ".tiff"}
-        method = "orb_resnet"
+        is_resnet = True
 
         # Чтение изображений из папок
         train_images_private, test_images_private = read_images_from_directory(
             train_images_path_private, test_images_path_private, image_extensions
         )
 
-        output_path_csv = Path(
-            f"{output_dir_path}/pred_{method}_final_submission.csv"
+        output_path_orb_csv = Path(
+            f"{output_dir_path}/pred_orb_submission.csv"
         )
-        output_path_txt = Path(f"{output_dir_path}/pred_{method}_final.txt")
+        output_path_orb_resnet_csv = Path(
+            f"{output_dir_path}/pred_orb_resnet_submission.csv"
+        )
+        output_path_orb_txt = Path(f"{output_dir_path}/pred_orb_final.txt")
+        output_path_orb_resnet_txt = Path(f"{output_dir_path}/pred_orb_resnet_final.txt")
 
         # Метод ORB (Oriented FAST and Rotated BRIEF)
         print("-------------------Метод ORB-------------------")
@@ -52,37 +56,51 @@ if __name__ == "__main__":
             threshold=40,
         )
 
-        print("-------------------ResNet50-------------------")
-        left_images = [img for img in test_images_private if img not in orb_preds]
-        IMAGE_SIZE = (512, 512)
-        embedder = TransformerEmbedder(IMAGE_SIZE, model_name="resnet50", device="cuda")
-
-        # Создание эмбеддингов
-        print("Создание эмбеддингов для тренировочных изображений...")
-        train_embeddings = embedder.compute_embeddings(train_images_private)
-
-        print("Создание эмбеддингов для тестовых изображений...")
-        test_embeddings = embedder.compute_embeddings(left_images)
-
-        resnet_preds = embedder.get_image_comparison(
-            train_embeddings=train_embeddings,
-            test_embeddings=test_embeddings,
-            test_paths=left_images,
-            threshold=0.85,
-        )
-
-        preds = orb_preds + resnet_preds
-
-        preds_labels = [1 if img in preds else 0 for img in test_images_private]
-        assert len(preds_labels) == len(
-            test_images_private
-        ), "Количество предсказаний не совпадает с количеством тестовых изображений"
-        test_images_private = [img.name for img in test_images_private]
+        preds_labels = [1 if img in orb_preds else 0 for img in test_images_private]
+        test_images_private_ = [img.name for img in test_images_private]
         save_paths_to_file(
-            output_path_csv, [test_images_private, preds_labels], format="csv"
+            output_path_orb_csv, [test_images_private_, preds_labels], format="csv"
         )
-        save_paths_to_file(output_path_txt, preds, format="txt")
+        save_paths_to_file(output_path_orb_txt, orb_preds, format="txt")
 
         end_time = time.time()
         final_time = time.strftime("%M:%S", time.gmtime(end_time - start_time))
-        print(f"Итоговое время выполнения: {final_time}")
+        print(f"Итоговое время выполнения ORB: {final_time}")
+
+
+        if is_resnet:
+            print("-------------------ResNet50-------------------")
+            left_images = [img for img in test_images_private if img not in orb_preds]
+            IMAGE_SIZE = (512, 512)
+            embedder = TransformerEmbedder(IMAGE_SIZE, model_name="resnet50", device="cuda")
+
+            # Создание эмбеддингов
+            print("Создание эмбеддингов для тренировочных изображений...")
+            train_embeddings = embedder.compute_embeddings(train_images_private)
+
+            print("Создание эмбеддингов для тестовых изображений...")
+            test_embeddings = embedder.compute_embeddings(left_images)
+
+            resnet_preds = embedder.get_image_comparison(
+                train_embeddings=train_embeddings,
+                test_embeddings=test_embeddings,
+                test_paths=left_images,
+                threshold=0.85,
+            )
+
+            preds = orb_preds + resnet_preds
+
+            preds_labels = [1 if img in preds else 0 for img in test_images_private]
+            assert len(preds_labels) == len(
+                test_images_private
+            ), "Количество предсказаний не совпадает с количеством тестовых изображений"
+
+            test_images_private = [img.name for img in test_images_private]
+            save_paths_to_file(
+                output_path_orb_resnet_csv, [test_images_private, preds_labels], format="csv"
+            )
+            save_paths_to_file(output_path_orb_resnet_txt, preds, format="txt")
+
+            end_time = time.time()
+            final_time = time.strftime("%M:%S", time.gmtime(end_time - start_time))
+            print(f"Итоговое время выполнения метода ORB + ResNet50: {final_time}")
